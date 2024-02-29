ba1ca6047ab155117275001f7a3744905d4dc5f5
Emma Boland <emmomp@bas.ac.uk>
2024-02-29 10:54:14 +0000

    added TypeError for non-arrays to patient_normalise and test

--- inflammation/models.py
+++ inflammation/models.py
@@ -55,6 +55,8 @@ def patient_normalise(data):
     """
     if np.any(data < 0):
         raise ValueError('Inflammation values should not be negative')
+    if not isinstance(data, np.ndarray):
+        raise TypeError('Inflammation data should be in an array')
     max = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
         normalised = data / max[:, np.newaxis]
